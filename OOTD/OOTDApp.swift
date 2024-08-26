0d07ffb31afa2e36d30d656c92b201f37d19e3fe
hrsma2i <hrs.ma2i@gmail.com>
2024-08-26 22:42:53 +0900

    Initial Commit

---
+++ OOTD/OOTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOTDApp.swift
+//  OOTD
+//
+//  Created by Hiroshi Matsui on 2024/08/26.
+//
+
+import SwiftUI
+
+@main
+struct OOTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
