4b79d6583ad813b8020be75b0a1efe810eaf2e28
hrsma2i <hrs.ma2i@gmail.com>
2024-09-09 22:45:58 +0900

    import items from web via Google search

--- OOTD/Views/Item/ItemAddSelectWebSiteScreen.swift
+++ OOTD/Views/Item/ItemAddSelectWebSiteScreen.swift
@@ -8,19 +8,14 @@
 import SwiftUI
 
 struct ItemAddSelectWebSiteScreen: HashableView {
+    @State private var searchQuery: String = ""
     @EnvironmentObject var navigation: NavigationManager
     @EnvironmentObject var itemStore: ItemStore
 
     func siteButton(_ name: String, url: String) -> some View {
         return Button {
             navigation.path.append(
-                ImageImportWebView(url: url) { imageUrls, _ in
-                    let items = imageUrls.map { Item(imageURL: $0) }
-                    navigation.path = NavigationPath()
-                    navigation.path.append(
-                        ItemDetail(items: items)
-                    )
-                }
+                ImageImportWebView(url: url, onSelected: passImagesToItemDetail)
             )
         } label: {
             Text(name)
@@ -32,9 +27,47 @@ struct ItemAddSelectWebSiteScreen: HashableView {
         Calendar.current.component(.year, from: Date())
     }
 
+    private func passImagesToItemDetail(imageUrls: [String], sourceUrl: String) {
+        let items = imageUrls.map { Item(imageURL: $0) }
+        navigation.path = NavigationPath()
+        navigation.path.append(
+            ItemDetail(items: items)
+        )
+    }
+
+    private var searchBar: some View {
+        let color = Color(red: 200/255, green: 200/255, blue: 200/255)
+        return HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(color)
+
+            TextField("Google で検索 / URL を入力", text: $searchQuery)
+                .onSubmit {
+                    let url: String
+                    if searchQuery.hasPrefix("https://") {
+                        url = searchQuery
+                    } else {
+                        url = "https://www.google.com/search?q=\(searchQuery)"
+                    }
+                    navigation.path.append(
+                        ImageImportWebView(url: url, onSelected: passImagesToItemDetail)
+                    )
+                }
+
+            Button {
+                searchQuery = ""
+            } label: {
+                Image(systemName: "multiply")
+                    .foregroundColor(color)
+            }
+        }
+    }
+
     var body: some View {
         List {
-            Section("インポート元") {
+            searchBar
+
+            Section("以下からインポート") {
                 siteButton("ZOZOTOWN", url: "https://zozo.jp/sp/_member/orderhistory/?ohid=&ohtype=2&baship=2&ohterm=\(currentYear)")
                 siteButton("GU", url: "https://www.gu-global.com/jp/ja/member/purchase/history")
                 siteButton("UNIQLO", url: "https://www.uniqlo.com/jp/ja/member/purchase/history")
