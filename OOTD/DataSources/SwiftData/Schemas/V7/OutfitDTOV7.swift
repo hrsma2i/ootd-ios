302aaf94a2bbede9a6b4d3f4927864e0088c8e4f
hrsma2i <hrs.ma2i@gmail.com>
2024-10-22 21:59:12 +0900

    just copy V6 as V7

---
+++ OOTD/DataSources/SwiftData/Schemas/V7/OutfitDTOV7.swift
@@ -0,0 +1,72 @@
+//
+//  OutfitDTOV7.swift
+//  OOTD
+//
+//  Created by Hiroshi Matsui on 2024/10/21.
+//
+
+import Foundation
+import SwiftData
+
+private let logger = getLogger(#file)
+
+extension SchemaV7 {
+    @Model
+    class OutfitDTO {
+        typealias ItemDTO = SchemaV7.ItemDTO
+        
+        @Attribute(.unique) var id: String
+        var items: [ItemDTO]
+        var tags: [String] = []
+        
+        // create 時のみ使う。 update, delete 時は .fetchSingle() を使う
+        // なぜなら、すでに container 内に保存済みのDTOと同じ id のDTOを生成すると、 DTO.id の参照時に EXC_BREAKPOINT のエラーが発生してしまうから。
+        // この原因は id に @Attribute(.unique) 制約があるから。なので、すでに保存済み（update, delete）の場合は container から取得する。
+        init(outfit: Outfit) {
+            id = outfit.id
+            
+            items = []
+            do {
+                items = try SwiftDataItemDataSource.shared.fetch(items: outfit.items)
+            } catch {
+                logger.error("\(error)")
+            }
+            
+            tags = outfit.tags
+        }
+        
+        func update(from outfit: Outfit) throws {
+            items = try SwiftDataItemDataSource.shared.fetch(items: outfit.items)
+            tags = outfit.tags
+        }
+        
+        func toOutfit() throws -> Outfit {
+            let imagePath = Outfit.generateImagePath(id, size: Outfit.imageSize)
+            let thumbnailPath = Outfit.generateImagePath(id, size: Outfit.thumbnailSize)
+            
+            let imageSource: ImageSource?
+            let thumbnailSource: ImageSource?
+            do {
+                // check there are images in the storage
+                let _ = try LocalStorage.applicationSupport.loadImage(from: imagePath)
+                let _ = try LocalStorage.applicationSupport.loadImage(from: thumbnailPath)
+                imageSource = .applicatinoSupport(imagePath)
+                thumbnailSource = .applicatinoSupport(thumbnailPath)
+            } catch {
+                imageSource = nil
+                thumbnailSource = nil
+            }
+            
+            return Outfit(
+                id: id,
+                // そのまま toItem による [Item] を items に渡したい
+                itemIds: items.compactMapWithErrorLog(logger) { itemDto in
+                    try itemDto.toItem().id
+                },
+                imageSource: imageSource,
+                thumbnailSource: thumbnailSource,
+                tags: tags
+            )
+        }
+    }
+}
---
+++ OOTD/DataSources/SwiftData/Schemas/V7/SchemaV7.swift
@@ -0,0 +1,19 @@
+//
+//  SchemaV7.swift
+//  OOTD
+//
+//  Created by Hiroshi Matsui on 2024/10/18.
+//
+
+import Foundation
+import SwiftData
+
+struct SchemaV7: VersionedSchema {
+    static var versionIdentifier = Schema.Version(7, 0, 0)
+    static var models: [any PersistentModel.Type] {
+        [
+            SchemaV7.ItemDTO.self,
+            SchemaV7.OutfitDTO.self
+        ]
+    }
+}
